ee03ae6e4b23ff2b2593d47f8172783198a26b8a
Robert Smith <rsmith@domain.sagedining.com>
2017-06-19 11:09:16 -0400

    bounce response back to sage

--- connection.php
+++ connection.php
@@ -16,26 +16,9 @@ $err = curl_error($curl);
 curl_close($curl);
 
 if ($err) {
-    echo "cURL Error #:" . $err;
+    return "cURL Error #:" . $err;
 } else {
-    echo $response;
+    return $response;
 }
 ?>
 
-//end sending
-
-/*
-//send to sage
-curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://bseipp-sagetest.cs67.force.com/services/apexrest/Account/get",
-    CURLOPT_SSLVERSION => 6,
-    CURLOPT_SSL_VERIFYPEER => true,
-    CURLOPT_RETURNTRANSFER => true,
-));
-
-if ($err) {
-    echo "cURL Error #:" . $err;
-} else {
-    echo $response;
-}
-*/

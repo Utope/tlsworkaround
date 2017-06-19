87afae297e37dc4b04360f5d302d034d2d9bb559
Robert Smith <rsmith@domain.sagedining.com>
2017-06-19 11:03:19 -0400

    fixed url error

--- connection.php
+++ connection.php
@@ -3,7 +3,7 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => "https://bseipp-sagetest.cs67.force.com/services/apexrest/Account/get",
+    CURLOPT_URL => "https://bseipp-sagetest.cs67.force.com/services/apexrest/RestController/get?sObjectType=Account",
     CURLOPT_SSLVERSION => 6,
     CURLOPT_SSL_VERIFYPEER => true,
     CURLOPT_RETURNTRANSFER => true,
@@ -20,6 +20,7 @@ if ($err) {
 } else {
     echo $response;
 }
+?>
 
 //end sending
 

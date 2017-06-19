8f1334805a1698f98523c2ec6711568ae66fe1f6
Robert Smith <rsmith@domain.sagedining.com>
2017-06-19 11:12:33 -0400

    f

--- connection.php
+++ connection.php
@@ -5,7 +5,6 @@ $curl = curl_init();
 curl_setopt_array($curl, array(
     CURLOPT_URL => "https://bseipp-sagetest.cs67.force.com/services/apexrest/RestController/get?sObjectType=Account",
     CURLOPT_SSLVERSION => 6,
-    CURLOPT_SSL_VERIFYPEER => true,
     CURLOPT_RETURNTRANSFER => true,
 ));
 
@@ -18,7 +17,5 @@ curl_close($curl);
 if ($err) {
     return "cURL Error #:" . $err;
 } else {
-    return $response;
+    return "bwah" . $response;
 }
-?>
-
